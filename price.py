e2de6943ce99f2ee9f07bf10e95d80b1c9ab4c3c
wanyi56 <wanyiyan@usc.edu>
2022-04-21 05:10:56 -0700

    modified

--- price.py
+++ price.py
@@ -4,20 +4,26 @@
 # In[3]:
 
 from selenium import webdriver
-import chromedriver_binary
+from selenium.webdriver import FirefoxOptions
 from bs4 import BeautifulSoup
 import pandas as pd
 import sys
 import ray
 import os
-
+import streamlit as st
 # In[8]:
 
 
-options = webdriver.ChromeOptions()
-options.add_argument('--headless')
-options.add_argument('--ignore-certificate-errors')
-options.add_argument('--allow-running-insecure-content')
+@st.experimental_singleton
+def installff():
+    os.system('sbase install geckodriver')
+    os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
+
+_ = installff()
+opt = webdriver.FirefoxOptions()
+opt.add_argument('--headless')
+opt.add_argument('--ignore-certificate-errors')
+opt.add_argument('--allow-running-insecure-content')
 user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
 options.add_argument(f'user-agent={user_agent}')
 
@@ -27,12 +33,12 @@ options.add_argument(f'user-agent={user_agent}')
 
 @ray.remote
 def chewy(k):
-    
+
 
     # chewy
-    driver_chewy = webdriver.Chrome(executable_path = "chromedriver",  chrome_options=options)
+    driver_chewy = webdriver.Firefox(options=opt)
     # chewy
-    
+
     url_chewy = "https://www.chewy.com/s?query=cat%20can%20"+k+"&nav-submit-button="
     driver_chewy.get(url_chewy)
     website_chewy = []
@@ -46,25 +52,25 @@ def chewy(k):
         for a in n[:min(5,len(n))-1]:
             price=a['data-price']
             name=a['data-name']
-        
+
             website_chewy.append('chewy')
             prices_chewy.append('$'+price)
-            names_chewy.append(name.strip()) 
+            names_chewy.append(name.strip())
 
     else:
         website_chewy = 'chewy'
         prices_chewy = 'no result'
         names_chewy = 'no result'
-    
-    
+
+
     d = {'Name':names_chewy,'Price':prices_chewy,'Sold by':website_chewy}
     df = pd.DataFrame(d)
-    
-    return [df,url_chewy]
-    
-       
 
-    
+    return [df,url_chewy]
+
+
+
+
 
 
 # In[39]:
@@ -72,10 +78,10 @@ def chewy(k):
 
 @ray.remote
 def petsmart(k):    #petsmart
-    driver_petsmart = webdriver.Chrome(executable_path = "chromedriver", chrome_options=options)
+    driver_petsmart = webdriver.Firefox(options=opt)
     url_petsmart = "https://www.petsmart.com/search/?q=cat%20wet%20food%20"+k+"&ps=undefined"
     driver_petsmart.get(url_petsmart)
-    
+
     website_petsmart = []
     prices_petsmart = []
     names_petsmart = []
@@ -86,23 +92,23 @@ def petsmart(k):    #petsmart
     if len(n) != 0:
         for a in n[:min(5,len(n))-1]:
             price=a.find('span', attrs={'class':'price-regular'})
-            if not price: 
+            if not price:
                 price=a.find('span', attrs={'class':'price-sales'})
             name=a.find('div', attrs={'class':'product-name'})
-        
+
             website_petsmart.append('pet-smart')
             prices_petsmart.append('$'+price["data-gtm-price"])
-            names_petsmart.append(name.text.strip()) 
+            names_petsmart.append(name.text.strip())
 
     else:
         website_petsmart = 'petsmart'
         prices_petsmart = 'no result'
         names_petsmart = 'no result'
 
-    
+
     d = {'Name':names_petsmart,'Price':prices_petsmart,'Sold by':website_petsmart}
     df = pd.DataFrame(d)
-    
+
     return [df,url_petsmart]
 
 
@@ -111,7 +117,7 @@ def petsmart(k):    #petsmart
 
 @ray.remote
 def petco(k):    #petco
-    driver_petco = webdriver.Chrome("chromedriver", chrome_options=options)
+    driver_petco = webdriver.Firefox(options=opt)
 
 
 
@@ -131,23 +137,23 @@ def petco(k):    #petco
             name=a.find('div', attrs={'class':'product-name'})
             website_petco.append('Petco')
             prices_petco.append(price.text.strip())
-            names_petco.append(name.text.strip()) 
-        
+            names_petco.append(name.text.strip())
+
         names_petco = [a.replace("\n", " ") for a in names_petco]
     if len(n) == 0:
         website_petco = ['petco']
         prices_petco = ['no result']
         names_petco = ['no result']
 
-    
 
 
-    
-    
+
+
+
     d = {'Name':names_petco,'Price':prices_petco,'Sold by':website_petco}
     df = pd.DataFrame(d)
-    
-    return [df,url_petco] 
+
+    return [df,url_petco]
 
 
 # In[ ]:
@@ -155,15 +161,15 @@ def petco(k):    #petco
 
 def search_price(k):
     k = k.replace(' ','%20')
-    
+
     ray.shutdown()
-    
+
     ray.init()
     ret_id1 = chewy.remote(k)
     ret_id2 = petsmart.remote(k)
     ret_id3 = petco.remote(k)
     ret1, ret2, ret3 = ray.get([ret_id1, ret_id2, ret_id3])
-    
+
     result = [pd.concat([ret1[0],ret2[0],ret3[0]]),ret1[1],ret2[1],ret3[1]]
     return result
 
@@ -199,7 +205,3 @@ def search_price(k):
 
 
 # In[ ]:
-
-
-
-
